5f2a0d41aaafb848ddd162791c78ec8bacff860e
Flmstr <43917625+Flmstr@users.noreply.github.com>
2021-09-01 11:01:29 +0300

    Update SquareFigures.cs

--- 1TaskFigures/SquareFigures.cs
+++ 1TaskFigures/SquareFigures.cs
@@ -13,13 +13,13 @@ namespace SquareFigures
         public abstract double Square();
     }
 
-    class Triangle : Figure
+    public class Triangle : Figure
     {
         double a, b, c;
 
         public Triangle(double a, double b, double c)
         {
-            if (a == 0 ^ b == 0 ^ c == 0)
+            if (a == 0 | b == 0 | c == 0)
             {
                 throw new Exception("Сторона не может быть меньше или равнен 0");
             }
@@ -32,17 +32,15 @@ namespace SquareFigures
         }
         public override double Square()
         {
-            {
-                double p = (a + b + c) / 2;
-                return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-            }
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
         }
 
         public bool Rectangular()
         {
             if (Math.Pow(a,2) == Math.Pow(b, 2) + Math.Pow(c, 2)
-                ^ Math.Pow(b, 2) == Math.Pow(a, 2) + Math.Pow(c, 2)
-                ^ Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2))
+                | Math.Pow(b, 2) == Math.Pow(a, 2) + Math.Pow(c, 2)
+                | Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2))
             {
                 return true;
             }
@@ -53,7 +51,7 @@ namespace SquareFigures
         }
 
     }
-    class Circle : Figure
+    public class Circle : Figure
     {
         double r;
 
